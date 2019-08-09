b4222e7bc3bd54f97684121bf86b582c85381fc5
Sumudu Lakruwan <lakruwanpathirage01@gmail.com>
2019-08-09 10:24:02 +0530

    create marking portal of admin

---
+++ app/src/main/java/com/example/backrow/adminmarkingportal.java
@@ -0,0 +1,19 @@
+package com.example.backrow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class adminmarkingportal extends AppCompatActivity {
+    Button bt1;
+    Button bt2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adminmarkingportal);
+
+
+
+    }
+}
