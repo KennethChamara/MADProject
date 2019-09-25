4877ac161ccbae5e73996dd8b324c9611bc518b9
KennethChamara <https://github.com/KennethChamara/myapp.git>
2019-09-25 12:11:51 +0530

    intent was change in Quiz_option

--- app/src/main/java/com/example/backrow/Quiz_opition.java
+++ app/src/main/java/com/example/backrow/Quiz_opition.java
@@ -1,6 +1,8 @@
 package com.example.backrow;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,26 @@ public class Quiz_opition extends AppCompatActivity {
         setContentView(R.layout.activity_quiz_opition);
 
 
+        assert getSupportActionBar() != null;   //null check
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        btnChange = findViewById(R.id.btnChnage);
+        btnList = findViewById(R.id.btnlist);
+
+
+        btnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),addQuiz.class));
+            }
+        });
+        btnList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),ListQuiz.class));
+            }
+        });
 
     }
 
